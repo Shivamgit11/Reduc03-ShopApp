83f70de7eecabb7f709b16c2af0c12f27d4a75cf
shivam <mailtoshivam2002@gmail.com>
2022-12-03 21:19:45 +0530

    Task 4.20 Doing Network calls and andling response the right way

--- src/store/cartslics.js
+++ src/store/cartslics.js
@@ -7,6 +7,10 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -17,28 +21,26 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
-      }else {
+      } else {
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
-        const id = action.payload;
-        const existingItem = state.items.find(item => item.id === id);
-        state.totalQuantity--;
-        if(existingItem.quantity === 1) {
-            state.items = state.items.filter(item => item.id !== id);
-        }else {
-            existingItem.quantity--;
-            // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-        }
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
     },
   },
 });
 
 export const cartActions = cartSlice.actions;
-
 
 export default cartSlice;
